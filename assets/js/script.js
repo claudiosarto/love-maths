314e1b68983b655e5c6cea824c9a1bdc014df867
Claudio Sarto <claudiosarto@gmail.com>
2023-12-24 20:33:06 +0000

    feat: add division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,9 @@ function runGame(gameType){
     else if (gameType === "subtract"){
         displaySubtractQuestion(num1,num2);
     } 
+    else if (gameType === "division"){
+        displayDivisionQuestion(num1,num2);
+    } 
     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -88,6 +91,8 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-"){
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "÷"){
+        return [Math.floor(operand1 / operand2), "division"];
     } else {
         alert(`Unimplemented operator: ${operator}`);
         throw `Unimplemented operator: ${operator}. Aborting!`;
@@ -129,6 +134,9 @@ function displayMultiplyQuestion(operand1,operand2){
     document.getElementById("operator").textContent = "x";
 }
 
-function displayDivisionQuestion(){
+function displayDivisionQuestion(operand1,operand2){
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1:operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2:operand1;
+    document.getElementById("operator").textContent = "÷";
 
 }
